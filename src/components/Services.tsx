b45db7f42c738a8d582f25e3db9af7c0d755c0a4
Rhythm1821 <alamrawat1821@gmail.com>
2024-09-09 18:11:50 +0530

    Services component

---
+++ src/components/Services.tsx
@@ -0,0 +1,30 @@
+import { SERVICES_TEXT } from "@/constants";
+import Image from "next/image";
+
+export default function Services() {
+    return (
+        <div className="container mx-auto my-8 border-b pb-10">
+            <h2 className="mb-20 text-center text-3xl tracking-tighter sm:text-4xl lg:text-5xl">Services</h2>
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2">
+                    <div className="p-6">
+                        <Image
+                            className="rounded-xl object-cover"
+                            src="/service.jpeg"
+                            alt="Services"
+                            width={600}
+                            height={600} />
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2">
+                    <h2 className="mt-10 text-5xl lg:text-7xl">
+                        We are 
+                        <span className="leading-snug lg:block">available in</span>
+                        <span className="font-bold">42+ cities</span>
+                    </h2>
+                    <p className="max-w-xl py-6 text-xl tracking-tighter lg:pr-20">{SERVICES_TEXT}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
